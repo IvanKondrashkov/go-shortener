db53303c85c25d4d30c8d3a1b4ad2fd2c9ebfbfc
Ivan Kondrashkov <vanya.kondrashkov.92@mail.ru>
2024-11-25 14:58:34 +0300

    iter3: Добавлен инкремент 3

--- cmd/shortener/router.go
+++ cmd/shortener/router.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/IvanKondrashkov/go-shortener/internal/app"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewRouter(app *app.App) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, app.ShortenURL)
-	mux.HandleFunc(`/{id}`, app.GetURLByID)
-	return mux
+func NewRouter(app *app.App) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Route(`/`, func(r chi.Router) {
+		r.Post(`/`, app.ShortenURL)
+		r.Get(`/{id}`, app.GetURLByID)
+	})
+
+	return r
 }
